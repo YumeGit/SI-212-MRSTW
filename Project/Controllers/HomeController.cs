96838669ba87d8534dc48600bbf8334c53ffedfe
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2023-01-31 00:09:49 +0200

    Initial Commit po nastoyashemu

---
+++ Project/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int? id)
+        {
+            return View(new CringeData { id = id });
+        }
+
+        public ActionResult Cringe()
+        {
+            return View();
+        }
+	}
+
+	public class CringeData
+	{
+		public int? id;
+	}
+}
