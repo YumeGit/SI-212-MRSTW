db6c4183af73ce686ffeb5b785d29509e93549df
M1noka <maliaheiner@mail.ru>
2023-03-27 02:26:24 +0300

    CommentForm

---
+++ MRSTW.Web/Models/CommentForm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MRSTW.Web.Models
+{
+	public class CommentForm
+	{
+		[Required]
+		public int PostId { get; set; }
+		[Required]
+		[MaxLength(100)]
+		public string Message { get; set; }
+	}
+}
