4bb9c885d023b9d6c22f8b7a77162389839fd5eb
M1noka <maliaheiner@mail.ru>
2023-03-27 02:26:15 +0300

    BaseBlogController

---
+++ MRSTW.Web/Controllers/BaseBlogController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace MRSTW.Controllers
+{
+	public abstract class BaseBlogController : Controller
+	{
+		public HttpStatusCodeResult HttpNoPermission() => new HttpStatusCodeResult(403);
+	}
+}
