fae0231ee41aded7419fce5972d6d5ea5ce6a882
M1noka <moonlydays1@gmail.com>
2023-03-27 01:22:22 +0300

    Added Forms for Auth Views

---
+++ MRSTW.Web/Models/LoginForm.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MRSTW.Web.Models
+{
+	public class LoginForm
+	{
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string Password { get; set; }
+	}
+}
---
+++ MRSTW.Web/Models/RegisterForm.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MRSTW.Web.Models
+{
+	public class RegisterForm
+	{
+		[Required]
+		[StringLength(32)]
+		public string Name { get; set; }
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+		[Required]
+		[MaxLength(32)]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+		[Required]
+		[MaxLength(32)]
+		[DataType(DataType.Password)]
+		public string PasswordConfirm { get; set; }
+	}
+}
